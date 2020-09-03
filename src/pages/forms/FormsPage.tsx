48f125c063d71914dc4b6651b18cf0f8209ac263
maxidela <maxidela@gmail.com>
2020-09-03 17:41:31 -0300

    Adding loading spinner

--- src/pages/forms/FormsPage.tsx
+++ src/pages/forms/FormsPage.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import FormList from "./FormsList";
+import Spinner from "../../shared/Spinner";
 import { useSelector, useDispatch } from "react-redux";
 import { loadForms, deleteForm } from "../../state/actions/formActions";
 import { form } from "../../state/actions/actionTypes";
@@ -19,7 +20,11 @@ const FormsPage = () => {
   return (
     <>
       <h2>List of Forms</h2>
-      <FormList forms={forms} onDelete={onDelete} />
+      {!forms.length ? (
+        <Spinner />
+      ) : (
+        <FormList forms={forms} onDelete={onDelete} />
+      )}
     </>
   );
 };
